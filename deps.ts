724a1c37bcd9d388a963495a192d887244f81e3b
i-xi-dev <naoyuki.isono@gmail.com>
2024-06-17 01:56:06 +0900

    1.0.1

--- deps.ts
+++ deps.ts
@@ -1,13 +1,13 @@
 export {
   CodePoint,
   RuneString,
-} from "https://raw.githubusercontent.com/i-xi-dev/unicode.es/1.0.0-beta.3/mod.ts";
-export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/7.0.0/mod.ts";
-export { TextEncoding } from "https://raw.githubusercontent.com/i-xi-dev/text-encoding.es/6.3.8/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/unicode.es/1.0.1/mod.ts";
+export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/7.0.1/mod.ts";
+export { TextEncoding } from "https://raw.githubusercontent.com/i-xi-dev/text-encoding.es/6.3.9/mod.ts";
 export {
   Radix,
   Uint16,
   Uint32,
   Uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.0.1/mod.ts";
-export { SafeIntegerFormat } from "https://raw.githubusercontent.com/i-xi-dev/number-format.es/1.0.3/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.1.0/mod.ts";
+export { SafeIntegerFormat } from "https://raw.githubusercontent.com/i-xi-dev/number-format.es/1.0.4/mod.ts";
